563bee04eac524e04d0e8166b4a7b28dbf1b578f
Jonasson Oscar <oscjjonasson@gmail.com>
2022-08-22 10:42:18 +0300

    added Nav component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
-
+import Nav from '../Nav/Nav';
 const Header = () => {
   return (
     <header>
