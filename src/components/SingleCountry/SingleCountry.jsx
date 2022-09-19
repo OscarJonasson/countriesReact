25d654e23fe2d50dd4db74b8e05310f40a28f152
Jonasson Oscar <oscjjonasson@gmail.com>
2022-09-19 13:26:30 +0300

    hover effect for favheart

--- src/components/SingleCountry/SingleCountry.jsx
+++ src/components/SingleCountry/SingleCountry.jsx
@@ -57,7 +57,7 @@ const SingleCountry = () => {
             width="40"
             height="40"
             fill="currentColor"
-            class="bi bi-skip-backward-btn"
+            className="bi bi-skip-backward-btn"
             viewBox="0 0 16 16"
           >
             <path d="M11.21 5.093A.5.5 0 0 1 12 5.5v5a.5.5 0 0 1-.79.407L8.5 8.972V10.5a.5.5 0 0 1-.79.407L5 8.972V10.5a.5.5 0 0 1-1 0v-5a.5.5 0 0 1 1 0v1.528l2.71-1.935a.5.5 0 0 1 .79.407v1.528l2.71-1.935z" />
