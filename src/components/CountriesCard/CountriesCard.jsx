0f6cce98ed9175d6f55651a1302e0a9f1b99d648
ronttizz <toni.nissinen@gmail.com>
2022-10-03 14:47:30 +0300

    population fixed when less than 1000

--- src/components/CountriesCard/CountriesCard.jsx
+++ src/components/CountriesCard/CountriesCard.jsx
@@ -1,37 +1,31 @@
-import React from 'react';
-import classes from './CountriesCard.module.css';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  addFavorite,
-  removeFavorite,
-} from '../../features/countries/cartSlice';
+import React from "react";
+import classes from "./CountriesCard.module.css";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorite, removeFavorite } from "../../features/countries/cartSlice";
 
 const CountriesCard = ({ country }) => {
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.cart);
 
   const populationRounding = () => {
+    if (country?.population < 1000) return country?.population;
     const population = (country?.population / 1000000).toPrecision(2);
-    return population >= 1
-      ? population + 'M'
-      : Math.round(population * 1000) + 'K';
+    return population >= 1 ? population + "M" : Math.round(population * 1000) + "K";
   };
 
   return (
     <Card
       className={classes.strapcard}
       border="danger"
-      style={{ width: '20rem' }}
+      style={{ width: "20rem" }}
       bg="dark"
     >
       <Card.Header>{country?.name?.common}</Card.Header>
       <Card.Body>
-        {favorites
-          .map((name) => name.name.common)
-          .includes(country.name?.common) ? (
+        {favorites.map((name) => name.name.common).includes(country.name?.common) ? (
           <button
             className={classes.favButRem}
             type="button"
@@ -73,32 +67,25 @@ const CountriesCard = ({ country }) => {
         <h3>Languages:</h3>
         <div className={classes.info}>
           {Object.values(country?.languages || {}).map((language, i) => (
-            <span key={i}>{(i ? ', ' : '') + language}</span>
+            <span key={i}>{(i ? ", " : "") + language}</span>
           ))}
         </div>
         <h3>Currency:</h3>
         <div className={classes.info}>
           {Object.values(country?.currencies || {}).map((currency, i) => (
-            <span key={i}>{(i ? ', ' : '') + currency.name}</span>
+            <span key={i}>{(i ? ", " : "") + currency.name}</span>
           ))}
         </div>
         <h3>Population</h3>
         <div className={classes.info}>{populationRounding()}</div>
         <div className={classes.info__seeMore}>
-          <Link
-            to={`/countries/${country?.name?.common}`.toLowerCase()}
-            state={country}
-          >
+          <Link to={`/countries/${country?.name?.common}`.toLowerCase()} state={country}>
             <Button className={classes.info__seeMore} variant="info">
               See more
             </Button>
           </Link>
         </div>
-        <img
-          className={classes.card__flag}
-          src={country?.flags?.svg}
-          alt="Flag"
-        />
+        <img className={classes.card__flag} src={country?.flags?.svg} alt="Flag" />
       </Card.Body>
     </Card>
   );
